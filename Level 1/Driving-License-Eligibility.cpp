68048fb6133c9911392788cf683833ef55c0f52b
Abdullah Almakhzomi <bsalm2791@gmail.com>
2026-02-25 23:12:57 +0300

    Add files via upload

---
+++ Level 1/Driving-License-Eligibility.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct stInfo {
+	short Age;
+	bool HasDrivingLicense;
+
+};
+
+
+stInfo ReadInfo() {
+	stInfo Info;
+
+	cout << "Enter Your Age : ";
+	cin >> Info.Age;
+
+	cout << "Do you has driver License? [0]No , 1[Yes]\n";
+	cin >> Info.HasDrivingLicense;
+
+	return Info;
+}
+
+
+bool TheCheckAgeIfWasGreaterThan21(stInfo Info)
+{
+	return (Info.Age > 21);
+}
+
+bool CheckDrivingLicense(stInfo Info)
+{
+	return (Info.HasDrivingLicense == true);
+}
+
+void PrintResult(stInfo Info)
+{
+	cout << "-------------------------\n";
+	if (TheCheckAgeIfWasGreaterThan21(Info) && CheckDrivingLicense(Info))
+	{
+		cout << "\nHired \n";
+	}
+	else
+	{
+		cout << "\nRejected \n";
+	}
+}
+
+int main()
+{
+	PrintResult(ReadInfo());
+}
+
